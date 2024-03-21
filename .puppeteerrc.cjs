fbe71ef37abdf31d1a8e1349911f12a6574d9b4a
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-03-21 00:56:52 -0500

    updated puppeteerrc

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,9 +5,5 @@ const { join } = require("path");
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(
-    __dirname,
-    process.env.PUPPETEER_CACHE || ".cache",
-    "puppeteer"
-  ),
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
 };
